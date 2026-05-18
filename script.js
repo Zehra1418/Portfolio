1dc2cf27ce46f4ca41bfa3b8089778f3a10ad071
Zehra1418 <fathimazehra248@gmail.com>
2026-05-18 12:22:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// MOBILE MENU
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// CLOSE MENU WHEN LINK CLICKED
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
+
